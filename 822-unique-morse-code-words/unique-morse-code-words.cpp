c2bc5e7bce4cb58483b0fdd806f9080346031ae1
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-12 22:35:55 +0530

    Time: 1 ms (32.31%) | Memory: 11.4 MB (78.25%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.cpp
+++ 822-unique-morse-code-words/unique-morse-code-words.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    void convert(unordered_set<string>&ans,string curr){
- map<char,string> m={{'a',".-"},{'b',"-..."},{'c',"-.-."},{'d',"-.."},{'e',"."},{'f',"..-."},{'g',"--."},{'h',"...."},{'i',".."},{'j',".---"},{'k',"-.-"},{'l',".-.."},{'m',"--"},{'n',"-."},{'o',"---"},{'p',".--."},{'q',"--.-"},{'r',".-."},{'s',"..."},{'t',"-"},{'u',"..-"},{'v',"...-"},{'w',".--"},{'x',"-..-"},{'y',"-.--"},{'z',"--.."}};
- string news="";
-        for(int i=0;i<curr.size();i++){
-            news+=m[curr[i]];
-        }
-        ans.insert(news);
-    }
     int uniqueMorseRepresentations(vector<string>& words) {
- unordered_set<string>ans;
+ unordered_set<string>unq;
+  vector<string> m = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
         for(int i=0;i<words.size();i++){
-            convert(ans,words[i]);
+            string news="";
+            for(char x : words[i]){
+                news+=m[x-'a'];
+            }
+            unq.insert(news);
         }
-        return ans.size();
+        return unq.size();
 
     }
 };

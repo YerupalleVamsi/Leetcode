78a67c5555f5224ffaeb6eb4c14b31da5db02e35
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-14 21:45:04 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (59.13%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         k=k%n;
         if(k==0)return;
-             reverse(nums.begin(), nums.end());          
+         reverse(nums.begin(), nums.end());          
         reverse(nums.begin(), nums.begin() + k);     
         reverse(nums.begin() + k, nums.end());
     }

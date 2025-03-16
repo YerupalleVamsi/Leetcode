2faa51a2fb3ba57788866ae11868915ef6950fb8
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-16 14:07:19 +0530

    Time: 713 ms (50.74%) | Memory: 21.9 MB (49.42%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.py
@@ -5,7 +5,7 @@ class Solution:
         right = cars*cars*ranks[0]
         while left < right:
             mid = (left + right) // 2
-            cars_repaired=sum(int((mid/rank)**0.5) for rank in ranks)
+            cars_repaired=sum(isqrt(mid//rank) for rank in ranks)
             if cars_repaired<cars:
                 left=mid+1
             else:

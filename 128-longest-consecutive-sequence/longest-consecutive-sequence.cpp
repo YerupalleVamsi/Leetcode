200e845fdfb9afb2e72340ff1f1641070285cf8e
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-22 18:00:34 +0530

    Time: 79 ms (70.50%) | Memory: 88.8 MB (29.62%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,30 +1,30 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-    if (nums.empty()) return 0;
-        unordered_map<int, int> mp;
-        int cnt = 0, maxCount = 0;
-        for (int num : nums) {
-            mp[num] = 0;
-        }
-        for (int num : nums) {
-            if (mp.find(num) != mp.end()) {
-                mp[num]++;
-            }
-        }
-        for (auto& [key, value] : mp) {
-            if (mp.find(key - 1) == mp.end()) {
-                int currentNum = key;
-                cnt = 0;
-                while (mp.find(currentNum) != mp.end() && mp[currentNum] >0)               {
-                    cnt++;
+        if (nums.empty()) return 0;
+
+        unordered_set<int> numSet(nums.begin(), nums.end()); 
+        int longestStreak = 0;
+
+    for (int num : numSet) {
+            if (numSet.find(num - 1) == numSet.end()) { 
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.find(currentNum + 1) != numSet.end()) {
                     currentNum++;
+                    currentStreak++;
                 }
 
-                maxCount = max(maxCount, cnt);
+                longestStreak = max(longestStreak, currentStreak);
             }
         }
 
-        return maxCount;
+        return longestStreak;
     }
 };

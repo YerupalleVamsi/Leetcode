5bb345f2308a5eabc267010394a135660b211f00
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-24 18:15:13 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (25.97%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+    int cnt=0;
+    int j=0;
+    string ans;
+    for(char c:s){
+        if(c=='('){
+            if(cnt>0)ans+=c;
+            cnt++;
+        }
+        else{
+            cnt--;
+           if(cnt>0) ans+=c;
+        }
+    }
+return  ans;
+
+    }
+};

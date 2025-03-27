61555f5a81f095475ac885938be2d72d113cac55
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-27 17:31:59 +0530

    Time: 45 ms (90.10%) | Memory: 31.9 MB (90.59%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        major=None
+        count=0
+        for num in nums:
+            if count==0:
+                major=num
+            count+=(1 if num == major else -1)
+        totalcount=nums.count(major)    
+        if totalcount<=len(nums)//2:
+            return -1
+        dominant=major
+        leftcount=0
+        for i in range(len(nums)):
+            if nums[i]==dominant:
+                leftcount+=1
+            rightcount=totalcount-leftcount 
+            if leftcount>(i+1)//2 and rightcount>(len(nums)-(i+1))//2:
+                return i
+        return -1           
+
+
+
+        

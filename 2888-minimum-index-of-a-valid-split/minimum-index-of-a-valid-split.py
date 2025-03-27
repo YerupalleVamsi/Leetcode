f1209dc883fb196c3bebe71219f5fa6940e97b04
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-27 17:33:22 +0530

    Time: 42 ms (94.55%) | Memory: 31.7 MB (98.02%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -6,9 +6,12 @@ class Solution:
             if count==0:
                 major=num
             count+=(1 if num == major else -1)
-        totalcount=nums.count(major)    
+
+        totalcount=nums.count(major) 
+
         if totalcount<=len(nums)//2:
             return -1
+            
         dominant=major
         leftcount=0
         for i in range(len(nums)):
@@ -17,4 +20,4 @@ class Solution:
             rightcount=totalcount-leftcount 
             if leftcount>(i+1)//2 and rightcount>(len(nums)-(i+1))//2:
                 return i
-        return -1                   
+        return -1            

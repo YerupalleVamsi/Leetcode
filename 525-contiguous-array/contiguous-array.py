b4d92b237cdf75c2c728b12b4600b28ee1f60bc6
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-28 14:39:22 +0530

    Time: 101 ms (64.75%) | Memory: 23.2 MB (97.37%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -7,12 +7,10 @@ class Solution:
         diff = 0
         for i, num in enumerate(nums):
             diff += 1 if num == 1 else -1  
-
             if diff in index_map:
                 max_length = max(max_length, i - index_map[diff]) #so if i get the same diff again ill update the length 
             else:
                 index_map[diff] = i  # Store first occurrence only
-
         return max_length
 
             

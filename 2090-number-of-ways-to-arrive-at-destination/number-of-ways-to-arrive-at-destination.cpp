b0a4863d2f7c27f35353992872069c0d3aea4557
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-31 01:22:51 +0530

    Time: 13 ms (27.76%) | Memory: 37.1 MB (36.39%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<pair<int, int>> gr[n];
+        for (auto it : roads) {
+            gr[it[0]].push_back({it[1], it[2]});
+            gr[it[1]].push_back({it[0], it[2]});
+        }
+
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+        
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        int mod = (int)(1e9 + 7);
+
+        while (!pq.empty()) {
+            long long dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if (dis > dist[node]) continue;
+
+            for (auto it : gr[node]) {
+                int adjNode = it.first;
+                int weight = it.second;
+
+                if (dis + weight < dist[adjNode]) {
+                    dist[adjNode] = dis + weight;
+                    pq.push({dist[adjNode], adjNode});
+                    ways[adjNode] = ways[node];
+                } else if (dis + weight == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+
+        return ways[n - 1] % mod;
+    }
+};

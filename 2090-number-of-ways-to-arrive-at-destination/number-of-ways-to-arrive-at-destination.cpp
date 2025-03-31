47f2ed74ae4db6a5e05c2c75b0d979e4b96d5114
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-03-31 09:44:17 +0530

    Time: 14 ms (25.64%) | Memory: 35.2 MB (43.98%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<int, int>> gr[n];
-        for (auto it : roads) {
-            gr[it[0]].push_back({it[1], it[2]});
-            gr[it[1]].push_back({it[0], it[2]});
+        unordered_map<int, vector<pair<int, int>>> gr;
+        for (auto& it : roads) {
+            gr[it[0]].emplace_back(it[1], it[2]);
+            gr[it[1]].emplace_back(it[0], it[2]);
         }
 
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
         vector<long long> dist(n, LLONG_MAX);
         vector<int> ways(n, 0);
         
         dist[0] = 0;
         ways[0] = 1;
-        pq.push({0, 0});
+        pq.emplace(0, 0);
 
-        int mod = (int)(1e9 + 7);
+        constexpr int mod = 1e9 + 7;
 
         while (!pq.empty()) {
-            long long dis = pq.top().first;
-            int node = pq.top().second;
+            auto [dis, node] = pq.top();
             pq.pop();
-            if (dis > dist[node]) continue;
-            for (auto it : gr[node]) {
-                int adjNode = it.first;
-                int weight = it.second;
 
-                if (dis + weight < dist[adjNode]) {
-                    dist[adjNode] = dis + weight;
-                    pq.push({dist[adjNode], adjNode});
+            if (dis > dist[node]) continue;  
+
+            for (auto& [adjNode, weight] : gr[node]) {
+                long long newDist = dis + weight;
+
+                if (newDist < dist[adjNode]) {
+                    dist[adjNode] = newDist;
+                    pq.emplace(newDist, adjNode);
                     ways[adjNode] = ways[node];
-                } else if (dis + weight == dist[adjNode]) {
+                } else if (newDist == dist[adjNode]) {
                     ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
                 }
             }

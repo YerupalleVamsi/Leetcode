311f75f408b4291189f77fa95d82ae400310bb70
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-04-13 09:41:38 +0530

    Add solution for Pow(x, n)

---
+++ LeetCode/Pow(x,-n)/Solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n; 
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double result = 1;
+        while (N > 0) {

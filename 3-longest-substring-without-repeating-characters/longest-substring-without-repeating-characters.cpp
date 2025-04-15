f0f601dccdb730c82219ec0e241bbbc7ce302303
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-04-15 22:28:53 +0530

    Time: 19 ms (31.63%) | Memory: 14.4 MB (15.62%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,12 +6,15 @@ public:
         int maxsize=1;
         int left=0;
         for(int i=0;i<s.size();i++){
+           if(seen.count(s[i])==0){ seen.insert(s[i]);
+            maxsize=max(maxsize,i-left+1);}
+            else{ 
             while(seen.count(s[i])){
                 seen.erase(s[left]);
                 left++;
+            } seen.insert(s[i]);
             }
-            seen.insert(s[i]);
-            maxsize=max(maxsize,i-left+1);            
+                       
         }
         return maxsize;
     }

6d205cb6cd10bdb2460a3223bd6a5ea3840d5a4c
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-04-18 20:12:36 +0530

    Add solution for Not Boring Movies

--- LeetCode/Not-Boring-Movies/Solution.sql
+++ LeetCode/Not-Boring-Movies/Solution.sql
@@ -1,2 +1,1 @@
-# Write your MySQL query statement below
-select id,movie,description,rating from Cinema where id%2=1 and description!='boring' order by rating desc;
+SELECT * FROM cinema WHERE (id % 2 = 1) AND (description != "boring") ORDER BY rating DESC;

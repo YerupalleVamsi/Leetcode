21cf5a97ebc3ec7e0a770a0be3d26842456f5b86
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-05-24 13:53:12 +0530

    Time: 2357 ms (10.24%) | Memory: 17.8 MB (87.79%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        hash=[0]*26
+        for ch in s1:
+            hash[ord(ch)-ord('a')]+=1
+        k=len(s1)
+        for i in range(len(s2)-k+1):
+            temphash=[0]*26
+            for j in range(i,i+k):
+                temphash[ord(s2[j])-ord('a')]+=1
+            if temphash==hash:
+                return True
+        return False
+
+
+
+        
+        

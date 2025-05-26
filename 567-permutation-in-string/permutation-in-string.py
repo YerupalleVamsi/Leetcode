0dfa18e0d32121815d86c8bf64363add1c416c59
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-05-26 14:15:56 +0530

    Time: 31 ms (44.11%) | Memory: 18.1 MB (18.70%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,19 +1,29 @@
 class Solution:
+    def isequal(self,a:List[int],b:List[int])-> bool:
+        for i in range(26):
+            if a[i]!=b[i]:
+                return False
+        return True
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1)>len(s2):
             return False
-        hash=[0]*26
+        hash1=[0]*26
+        hash2=[0]*26
         for ch in s1:
-            hash[ord(ch)-ord('a')]+=1
-        k=len(s1)
-        for i in range(len(s2)-k+1):
-            temphash=[0]*26
-            for j in range(i,i+k):
-                temphash[ord(s2[j])-ord('a')]+=1
-            if temphash==hash:
-                return True
+            hash1[ord(ch)-ord('a')]+=1
+        l,r=0,0
+        while(r<len(s2)):
+            hash2[ord(s2[r]) - ord('a')]+=1
+            if (r-l+1==len(s1)):
+                if(self.isequal(hash1,hash2)):
+                    return True
+            if (r-l+1<len(s1)):
+                r+=1
+            else:
+                hash2[ord(s2[l]) - ord('a')]-=1
+                l+=1
+                r+=1
         return False
-
 
 
         

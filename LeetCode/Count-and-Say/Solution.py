e79993dc6e5710500a13145cb6bdab253b82fa0a
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-05-27 09:19:42 +0530

    Add solution for Count and Say

--- LeetCode/Count-and-Say/Solution.py
+++ LeetCode/Count-and-Say/Solution.py
@@ -1,25 +1,15 @@
 class Solution:
-    def memory(self, n):  
-        map = []
-        count=1
-        for i in range(1,len(n)+1):
-            if i<len(n) and n[i]==n[i-1]:
-                count+=1
-            else:
-                map.append((count,n[i-1]))
-                count=1
-        return map
-
-    def count(self, map):  
-        ans = ""
-        for count,digit in map:
-            ans += str(count) +digit 
-        return ans
-
     def countAndSay(self, n: int) -> str:
         res = "1"
-        for _  in range(1,n):
-            map = self.memory(res) 
-            res = self.count(map)   
-            
+        for _ in range(1, n):
+            temp = ""
+            count = 1
+            for i in range(1, len(res) + 1):
+                if i < len(res) and res[i] == res[i - 1]:
+                    count += 1
+                else:
+                    temp += str(count) + res[i - 1]
+                    count = 1
+            res = temp
         return res
+

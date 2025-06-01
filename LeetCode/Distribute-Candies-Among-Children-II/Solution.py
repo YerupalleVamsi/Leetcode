39540ded050391b744bf5f294138c7589ca35d95
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-01 23:38:11 +0530

    Add solution for Distribute Candies Among Children II

--- LeetCode/Distribute-Candies-Among-Children-II/Solution.py
+++ LeetCode/Distribute-Candies-Among-Children-II/Solution.py
@@ -1,1 +1,11 @@
-Count the number of ways to give exactly x candies to 2 distinct children (Child A and Child B), where each child can receive anywhere from 0 up to limit.
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        ways=0
+        A_min=max(0,n-2*limit)
+        A_max=min(limit,n)
+        for i in range(A_min,A_max+1):
+            rem_candies=n-i
+            B_min=max(0,rem_candies-limit)
+            B_max=min(limit,rem_candies)
+            ways+=B_max-B_min+1
+        return ways

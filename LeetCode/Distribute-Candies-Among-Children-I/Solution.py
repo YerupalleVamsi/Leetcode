0e2648f3d0114b3c503a83f0db2ead435a834f1b
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-01 22:43:39 +0530

    Add solution for Distribute Candies Among Children I

--- LeetCode/Distribute-Candies-Among-Children-I/Solution.py
+++ LeetCode/Distribute-Candies-Among-Children-I/Solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
         count=0
-        for i in range(n+1):
-            for j in range(n+1):
-                for k in range(n+1):
-                    if(i+j+k==n and i<=limit and j<=limit and k<=limit):
+        for i in range(limit+1):
+            for j in range(limit+1):
+                for k in range(limit+1):
+                    if(i+j+k==n):
                         count+=1
         return count
 

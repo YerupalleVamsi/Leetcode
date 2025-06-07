4108bd48ed343510cce02d95a0b4798d565446a4
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-07 14:56:59 +0530

    Add solution for Lexicographically Minimum String After Removing Stars

--- LeetCode/Lexicographically-Minimum-String-After-Removing-Stars/Solution.cpp
+++ LeetCode/Lexicographically-Minimum-String-After-Removing-Stars/Solution.cpp
@@ -1,7 +1,21 @@
-"aaba*"
-"abc"
-"abc*de*fgh*"
-"a*b*c*d*"
-"abcde*f*"
-"abc*"
-"aaaa***"
+class Solution {
+public:
+    string clearStars(string s) {
+    priority_queue<pair<char,int>, vector<pair<char,int>>, greater<pair<char,int>>> minh;
+    unordered_map<int,bool> mpp;
+        for (int i = 0; i < s.size(); i++) {
+            if(s[i] == '*') {
+                mpp[-minh.top().second] = true;
+                minh.pop();
+            } else {
+                minh.push({s[i], -i});
+            }
+        }
+        string res;
+        for(int i = 0; i < s.size(); i++) {
+            if(mpp.count(i) || s[i] == '*') continue;
+            res += s[i];
+        }
+        return res;
+    }
+};

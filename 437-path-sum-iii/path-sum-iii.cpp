44eca11d2533681b9905a6b9eb863eb4123d6d9c
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-18 15:58:26 +0530

    Time: 19 ms (22.37%) | Memory: 18.8 MB (81.12%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -13,14 +13,12 @@ class Solution {
 public:
     int countPaths(TreeNode* root, long long targetSum) {
         if (!root) return 0;
-        
         int count = 0;
         if (root->val == targetSum) count++;
         count += countPaths(root->left, targetSum - root->val);
         count += countPaths(root->right, targetSum - root->val);
         return count;
     }
-    
     int pathSum(TreeNode* root, int targetSum) {
         if (!root) return 0;
         

1b34a02d3b269edf6b0e7f720881fe00befd5f01
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-28 19:53:12 +0530

    Add solution for Linked List Cycle

--- LeetCode/Linked-List-Cycle/Solution.cpp
+++ LeetCode/Linked-List-Cycle/Solution.cpp
@@ -1,23 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode * fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            fast=fast->next->next;
-            slow=slow->next;
-            if(fast==slow)
-            return true;
-        }
-        return false;
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-    }
-};
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast=head
+        slow=head
+        while(fast!=None and fast.next!=None):
+            slow=slow.next
+            fast=fast.next.next
+            if fast==slow:
+                return True
+        return False
+        

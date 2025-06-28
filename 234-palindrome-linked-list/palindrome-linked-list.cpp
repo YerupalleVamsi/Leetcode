c7983fc52ebcad08ec9a820e0089e2b662b08e2a
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-28 20:26:31 +0530

    Time: 0 ms (100.00%) | Memory: 118 MB (83.10%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,31 +1,41 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
+
+    // we will solve this in three steps nigga check it 
+
+    // read floyd's tortoise and hare algo also you will get it
     bool isPalindrome(ListNode* head) {
-        struct ListNode *temp= head;
-        vector<int>ll;
-        while(temp->next!=NULL){
-            ll.push_back(temp->val);
-            temp=temp->next;
+        if (!head || !head->next) return true;
+
+        // Step 1: Find middle (slow ends at middle)
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ll.push_back(temp->val);
-        int n=ll.size();
-        int l=n-1;
-        for(int i=0;i<n/2;i++){
-            if(ll[i]!=ll[l]){
+
+        // Step 2: Reverse second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+        while (curr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        // Step 3: Compare both halves
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = prev;
+        while (secondHalf) {
+            if (firstHalf->val != secondHalf->val) {
                 return false;
             }
-            l--;
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
         }
+
         return true;
     }
 };

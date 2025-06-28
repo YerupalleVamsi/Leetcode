fb819300fea50431322b4a3e5f7a815a080210a2
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-28 19:41:30 +0530

    Add solution for Middle of the Linked List

--- LeetCode/Middle-of-the-Linked-List/Solution.cpp
+++ LeetCode/Middle-of-the-Linked-List/Solution.cpp
@@ -1,17 +1,22 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if(head==None or head.next==None):
-            return head
-        else:
-            fast=head
-            slow=head
-            while fast and fast.next!=None:
-                slow=slow.next
-                fast=fast.next.next
-            return slow
-        
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+};

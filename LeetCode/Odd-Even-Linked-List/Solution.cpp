d88744efcb196bfd71cf6a74498d786d0ba2284c
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-06-29 11:37:29 +0530

    Add solution for Odd Even Linked List

--- LeetCode/Odd-Even-Linked-List/Solution.cpp
+++ LeetCode/Odd-Even-Linked-List/Solution.cpp
@@ -1,19 +1,25 @@
-class Solution {
-public:
-    ListNode* oddEvenList(ListNode* head) {
-        if(!head || !head->next || !head->next->next) return head;
-        
-        ListNode *odd = head;
-        ListNode *even = head->next;
-        ListNode *even_start = head->next;
-        
-        while(odd->next && even->next){
-            odd->next = even->next; //Connect all odds
-            even->next = odd->next->next;  //Connect all evens
-            odd = odd->next;
-            even = even->next;
-        }
-        odd->next = even_start;   //Place the first even node after the last odd node.
-        return head; 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* oddEvenList(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
     }
-};
+    struct ListNode *odd=head;
+    struct ListNode *even=head->next;
+     struct ListNode *temp=even;
+    while( even!=NULL&&even->next!=NULL){
+        odd->next=even->next;
+        odd=odd->next;
+       even->next=odd->next;
+        
+        even=even->next;
+    }odd->next=temp;
+    
+    
+    return head;
+}

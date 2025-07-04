8ed9ed45bf8b7e1faeb98566ac97b98299b495a6
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-07-04 16:50:32 +0530

    Add solution for String to Integer (atoi)

--- LeetCode/String-to-Integer-(atoi)/Solution.cpp
+++ LeetCode/String-to-Integer-(atoi)/Solution.cpp
@@ -8,3 +8,19 @@ public:
         bool isnegative=false;
         if(i<n&&(s[i]=='-'||s[i]=='+')){
             isnegative=(s[i]=='-');
+            i++;
+        }
+        while(i<n && isdigit(s[i])){
+            news.push_back(s[i]);
+            i++;
+        }
+        long long ans=0;
+        for(char ch:news){
+            ans=ans*10+(ch-'0');
+            if(!isnegative && ans>INT_MAX) return INT_MAX;
+            if(isnegative && -ans<INT_MIN) return INT_MIN;
+        }
+        if(isnegative) ans= -ans;
+        return (int) ans;
+    }
+};

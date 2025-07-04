76aa166b68abcd690ed0cc88d26a086dab2f4f03
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-07-04 20:03:24 +0530

    Time: 141 ms (5.06%) | Memory: 147.5 MB (5.00%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char>st;
+     for(int i=0;i<s.size();i++){
+        if(s[i]=='('){
+            st.push(s[i]);
+        }
+        if(!st.empty() && s[i]==')' && st.top()=='('){
+            st.pop();
+        }
+        else if(i>0 &&  s[i]==')' && st.empty())return false;
+     }
+     return st.empty();
+    }
+
+    void backtrack(vector<string>&res,int n,string curr){
+        if(curr.size()>n)return;
+        if(curr.size()==n && isValid(curr)){
+            res.push_back(curr);
+            return;
+        }
+        backtrack(res,n,curr+'(');
+        backtrack(res,n,curr+')');
+        return;
+    }
+    vector<string> generateParenthesis(int n) {
+        // first we start with "(" we dont have an other option
+        vector<string>res;
+        backtrack(res,2*n,"(");
+        return res;
+
+    }
+};

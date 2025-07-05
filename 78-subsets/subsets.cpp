65072d66e8819209521239d8c2907b1283ace48a
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-07-05 09:18:38 +0530

    Time: 2 ms (41.51%) | Memory: 15.5 MB (19.67%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void rec(vector<vector<int>>& res, vector<int>& nums, int idx, vector<int> currset) {
-        if (idx == nums.size()) {
-            res.push_back(currset);
-            return;
-        }
-
-        // Do not include current element
-        rec(res, nums, idx + 1, currset);
-
-        // Include current element
-        currset.push_back(nums[idx]);
-        rec(res, nums, idx + 1, currset);
+void rec(vector<vector<int>>&res,vector<int>&nums,int idx,vector<int>currset){
+    if(idx==nums.size()){
+        res.push_back(currset);
+        return;
     }
+    rec(res,nums,idx+1,currset);
+    currset.push_back(nums[idx]);
+    rec(res,nums,idx+1,currset);
+   
 
+}
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        rec(res, nums, 0, {});
+       vector<vector<int>>res;
+        rec(res,nums,0,{});
         return res;
+
     }
 };

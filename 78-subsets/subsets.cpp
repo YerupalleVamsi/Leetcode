22ee5ec1a73e68d892b6cd07cfbb209c383db32e
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-07-05 11:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (43.33%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-void rec(vector<vector<int>>&res,vector<int>&nums,int idx,vector<int>currset){
-    if(idx==nums.size()){
-        res.push_back(currset);
-        return;
-    }
-    rec(res,nums,idx+1,currset);
-    currset.push_back(nums[idx]);
-    rec(res,nums,idx+1,currset);
-}
+// we can solve it by bit manipulation
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<vector<int>>res;
-        rec(res,nums,0,{});
+        vector<vector<int>>res;
+        int n=nums.size();
+        for(int num=0;num<(1<<n);num++){
+            vector<int>sub;
+            for(int i=0;i<n;i++){
+                if((num&(1<<i))>0){
+                    sub.push_back(nums[i]);
+                }
+            }
+            res.push_back(sub);
+        }
         return res;
     }
 };

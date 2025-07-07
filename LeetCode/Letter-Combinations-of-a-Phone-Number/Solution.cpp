80a39b1ac92389a4f9b6ba004283e043e364900a
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-07-07 22:54:27 +0530

    Add solution for Letter Combinations of a Phone Number

---
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void backtrack(string &digits,string curr,unordered_map<char,string>&mpp,int currindex,vector<string>&ans){
+        if(curr.size()==digits.size()){
+            ans.push_back(curr);
+            return;
+        }
+        for(int i=currindex;i<digits.size();i++){
+                string next=mpp[digits[i]];
+                for(char ch:next){
+                    curr+=ch;
+                backtrack(digits,curr,mpp,i+1,ans);
+                curr.pop_back();}
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        vector<string>ans;
+        if(digits.size()==0)return ans;
+        unordered_map<char,string> mpp={
+            {'2',"abc"},
+            {'3',"def"},
+            {'4',"ghi"},
+            {'5',"jkl"},
+            {'6',"mno"},
+            {'7',"pqrs"},
+            {'8',"tuv"},
+            {'9',"wxyz"}
+        };
+        backtrack(digits,"",mpp,0,ans);
+        return ans;
+
+    }
+};

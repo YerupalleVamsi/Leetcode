7d67f6efaa05a3f4c1c760e56f619c05f4d67b23
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-07-13 14:37:45 +0530

    Time: 440 ms (25.79%) | Memory: 93.3 MB (56.75%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -9,7 +9,7 @@ public:
         }
 
         for (int i = index; i < num.size(); ++i) {
-            // Skip numbers with leading zeros
+            // for leading 0's
             if (i != index && num[index] == '0') break;
 
             string part = num.substr(index, i - index + 1);
@@ -21,7 +21,8 @@ public:
             } else {
                 backtrack(num, target, res, curr + "+" + part, eval + val, i + 1, val);
                 backtrack(num, target, res, curr + "-" + part, eval - val, i + 1, -val);
-                backtrack(num, target, res, curr + "*" + part, eval - prev + prev * val, i + 1, prev * val);
+                backtrack(num, target, res, curr + "*" + part, eval - prev + prev * val, i + 1, prev * val); // nigga this part is improtant 
+                // here in case of * we have precedance  ex:  3+4*5 ---> first we evaluated 3+4 --> 7 prev=4 , so we have to remove prev from eval || so , eval - prev --> this removes any addition or subtraction that has been done before || then we add prev* val that makes eval=7 and prev=4 --> 7-4=3 --> 3 + (4*5) = 3+20=23
             }
         }
     }

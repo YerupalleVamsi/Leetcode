14d92fe19b6940fe56afab6311424b7cda75f502
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-07-14 16:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (59.97%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -14,4 +14,8 @@ public:
    // 1)  __builtin_popcount(n) in cpp only
    // 2) use a while loop: while(n!=0){n=n&(n-1); cnt++;}
    // 3) use a left shift x&(1<<i) in a loop like used in the code
+   // 4) while(ans>0){
+   //        if(ans&1)cnt++;
+   //         ans=ans>>1;
+   //     } or like this using right shift
     

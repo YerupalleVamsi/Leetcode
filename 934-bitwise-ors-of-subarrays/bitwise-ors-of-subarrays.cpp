fa156d47dd87240bca827c2141f423230314684e
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-07-31 16:09:10 +0530

    Time: 1079 ms (54.28%) | Memory: 339.8 MB (42.12%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
@@ -7,12 +7,12 @@ public:
     int n=arr.size();
     for (int i = 0; i < n; ++i) {
         unordered_set<int> next;
-        next.insert(arr[i]);
+        next.insert(arr[i]); // start 
         for (int x : curr)
-            next.insert(x | arr[i]);
-        curr = next;  
+            next.insert(x | arr[i]); // or with prev set
+        curr = next;  // so in the next iteration we can use the curr to OR
         for (int x : curr)
-            result.insert(x);
+            result.insert(x); // store all the or vals
 }
 return result.size();
     }

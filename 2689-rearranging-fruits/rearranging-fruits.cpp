c2117f943debc92493788f205ebf987cdb4783fd
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-08-02 17:24:53 +0530

    Time: 170 ms (11.03%) | Memory: 122 MB (5.52%) - LeetSync

---
+++ 2689-rearranging-fruits/rearranging-fruits.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    long long minCost(vector<int>& basket1, vector<int>& basket2) {
+        unordered_map<int, int> mpp1, mpp2;
+        set<int> s;
+        int mini = INT_MAX; // to calc min element of both baskets
+
+        for (int i : basket1) {
+            mpp1[i]++;
+            s.insert(i);
+            mini = min(mini, i);
+        }
+        for (int i : basket2) {
+            mpp2[i]++;
+            s.insert(i);
+            mini = min(mini, i);
+        }
+
+        unordered_map<int, int> target; // the target freq
+        for (int i : s) {
+            int sum = mpp1[i] + mpp2[i];
+            if (sum & 1) return -1;
+            target[i] = sum / 2;
+        }
+
+        vector<int> excess1, excess2; // the excess ones 
+        for (auto& it : mpp1) {
+            int extra = it.second - target[it.first];
+            if (extra > 0) while (extra--) excess1.push_back(it.first);  // push excess elements 
+        }
+        for (auto& it : mpp2) {
+            int extra = it.second - target[it.first];
+            if (extra > 0) while (extra--) excess2.push_back(it.first); 
+        }
+
+        if (excess1.size() != excess2.size()) return -1; 
+
+        sort(excess1.begin(), excess1.end());
+        sort(excess2.rbegin(), excess2.rend()); // sort in descending order 
+
+        long long min_cost = 0;
+        for (int i = 0; i < excess2.size(); i++) {
+            int curr_min = min(excess1[i], excess2[i]);
+            min_cost += min(curr_min, 2 * mini); 
+        }
+
+        return min_cost;
+    }
+};

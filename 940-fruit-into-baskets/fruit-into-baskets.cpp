b8e78a9fc62d3e471e4b5221dd27a137570f0f86
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-08-04 07:26:01 +0530

    Time: 39 ms (63.30%) | Memory: 75.1 MB (57.10%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        if(fruits.size()<2) return fruits.size();
-        int left=0;
+        if(fruits.size()<2)return fruits.size();
+        int l=0;
         int max_length=1;
         unordered_map<int,int>freq;
-        for(int right=0;right<fruits.size();right++){
-            freq[fruits[right]]++;
-            while (freq.size()>2){
-                freq[fruits[left]]--;
-                if(freq[fruits[left]]==0)
-                    freq.erase(fruits[left]);
-                    left++;
+        for(int r=0;r<fruits.size();r++){
+            freq[fruits[r]]++;
+            while(freq.size()>2){
+                freq[fruits[l]]--;
+                if(freq[fruits[l]]==0)
+                freq.erase(fruits[l]);
+                l++;
             }
-            if(freq.size()<=2){
-            max_length=max(max_length,right-left+1);}
+            if(freq.size()<=2)
+            max_length=max(max_length,r-l+1);
         }
         return max_length;
     }

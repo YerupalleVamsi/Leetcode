dd5c0cc3445776a1723081384deaddc8ed10f74b
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-08-18 18:00:03 +0530

    Time: 135 ms (7.49%) | Memory: 47 MB (5.06%) - LeetSync

---
+++ 679-24-game/24-game.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    const double tar=24.0;
+    bool judgePoint24(vector<int>& cards) {
+        int n=cards.size();
+        vector<double>real(cards.begin(),cards.end());
+        return backtrack(real);
+    }
+    bool backtrack(vector<double>&real){
+        if(real.size()==1)return fabs(real[0]-tar)<1e-6;
+        for(int i=0;i<real.size();i++){
+            for(int j=0;j<real.size();j++){
+                if(i==j) continue;
+                vector<double>next;
+                for(int k=0;k<real.size();k++)
+                if(k!=i && k!=j)
+                next.push_back(real[k]);
+
+                for(double t: oper(real[i],real[j])){
+                    next.push_back(t);
+                    if(backtrack(next)) return true;
+                    next.pop_back();
+                }
+            }
+        }
+        return false;
+    }
+    unordered_set<double> oper(double a,double b){
+        unordered_set<double> res;
+        res.insert(a+b);
+        res.insert(a*b);
+        res.insert(a-b);
+        res.insert(b-a);
+        if(fabs(a) > 1e-6 )res.insert(b/a);
+        if(fabs(b)> 1e-6) res.insert(a/b);
+        return res;
+    }
+};

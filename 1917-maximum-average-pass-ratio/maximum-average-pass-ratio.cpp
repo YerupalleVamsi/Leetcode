1681e5967f8139765d5abfd443e380b97f9e09c1
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-09-01 13:05:41 +0530

    Time: 327 ms (38.01%) | Memory: 108.9 MB (21.05%) - LeetSync

---
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& c, int rem) {
+        auto cmp = [](const pair<double, pair<int, int>>& a, const pair<double, pair<int, int>>& b) {
+            return a.first < b.first; // dont be scared nigga this is a custom comparator for pq
+        };
+priority_queue<pair<double, pair<int, int>>, vector<pair<double, pair<int, int>>>, decltype(cmp)> pq(cmp);
+
+        for(auto i:c){
+            int pass=i[0];
+            int total=i[1];
+            double curr=(double)pass/total;
+            double diff=((double)(pass+1)/(total+1))-curr;
+            pq.push({diff,{pass,total}});
+        }
+        while(rem>0){
+            int pass=pq.top().second.first+1;
+            int total=pq.top().second.second+1;
+            pq.pop();
+            rem--;
+            double curr=(double)pass/total;
+            double diff=((double)(pass+1)/(total+1))-curr;
+            pq.push({diff,{pass,total}});
+        }
+        double total_ratio=0.0;
+        while(!pq.empty()){
+            double ratio=(double)pq.top().second.first/pq.top().second.second;
+            pq.pop();
+            total_ratio+=ratio;
+        }
+        return total_ratio/c.size();
+    }
+};

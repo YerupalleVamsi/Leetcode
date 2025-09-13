2b18775f1671a94a5c72ecd816928497f92b1641
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-09-13 09:40:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (99.13%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -6,16 +6,12 @@ public:
         f[c-'a']++;
         int v=0;
         int con=0;
-        string vows="aeiou";
-        for(char c:vows){
-        v=max(v,f[c-'a']);
-        }
-        cout << v <<" ";
         for(char c:s){
             if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u')
             con=max(con,f[c-'a']);
+            else
+            v=max(v,f[c-'a']);
         }
-        cout << con ;
         return v+con;
     }
 };

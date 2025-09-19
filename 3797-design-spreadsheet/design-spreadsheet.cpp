d1029065a7914bb880abfa6446fa338ed77b422c
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-09-19 06:36:23 +0530

    Time: 75 ms (75.27%) | Memory: 124.1 MB (87.50%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.cpp
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -1,76 +1,59 @@
 class Spreadsheet {
 public:
-    vector<vector<int>> sheet;
+    unordered_map<string,int> sheet;
 
-    Spreadsheet(int rows) {
-        sheet.assign(rows, vector<int>(26, 0));
-    }
-    
+    Spreadsheet(int rows) { }
+
     void setCell(string cell, int value) {
-        char col = cell[0];
-        string row = "";
-        for (int i = 1; i < cell.size(); i++)
-            row = row + cell[i];
-        sheet[stoi(row) - 1][col - 'A'] = value;
+        sheet[cell] = value;
     }
-    
+
     void resetCell(string cell) {
-        char col = cell[0];
-        string row = "";
-        for (int i = 1; i < cell.size(); i++)
-            row = row + cell[i];
-        sheet[stoi(row) - 1][col - 'A'] = 0;
+        sheet[cell] = 0;
     }
+
+    int getValue(string formula) {
+    int i = 1; 
+    string op1 = "";
+    while (i < formula.size() && formula[i] != '+') { 
+        op1 += formula[i];
+        i++;
+    }
+    i++; 
     
-     int getValue(string formula) {
-        int i = 1; 
-        string op1 = "";
-        while (i < formula.size() && formula[i] != '+') { 
-            op1 = op1 + formula[i];
-            i++;
-        }
-        i++; 
-        
-        string op2 = "";
-        while (i < formula.size()) {
-            op2 = op2 + formula[i];
-            i++;
-        }
-        if ((op1[0] >= 'A' && op1[0] <= 'Z') && (op2[0] >= 'A' && op2[0] <= 'Z')) {
-            char col1 = op1[0];
-            string row1 = "";
-            for (int i = 1; i < op1.size(); i++)
-                row1 = row1 + op1[i];
-            
-            char col2 = op2[0];
-            string row2 = "";
-            for (int i = 1; i < op2.size(); i++)
-                row2 = row2 + op2[i];
-            
-            int sum = sheet[stoi(row1) - 1][col1 - 'A'] + sheet[stoi(row2) - 1][col2 - 'A'];
-            return sum;
-        }
-        else if (op1[0] >= 'A' && op1[0] <= 'Z') {
-            char col1 = op1[0];
-            string row1 = "";
-            for (int i = 1; i < op1.size(); i++)
-                row1 = row1 + op1[i];
-            
-            int s2 = stoi(op2);
-            return sheet[stoi(row1) - 1][col1 - 'A'] + s2;
-        }
-        else if (op2[0] >= 'A' && op2[0] <= 'Z') {
-            char col2 = op2[0];
-            string row2 = "";
-            for (int i = 1; i < op2.size(); i++)
-                row2 = row2 + op2[i];
-            
-            int s1 = stoi(op1);
-            return s1 + sheet[stoi(row2) - 1][col2 - 'A'];
-        }
-        return stoi(op1) + stoi(op2);
+    string op2 = "";
+    while (i < formula.size()) {
+        op2 += formula[i];
+        i++;
     }
+
+    int val1 = 0, val2 = 0;
+
+    if (sheet.find(op1) != sheet.end()) {
+        val1 = sheet[op1];
+    } else {
+        bool num = !op1.empty();
+        for (char c : op1) {
+            if (!isdigit(c)) { num = false; break; }
+        }
+        if (num) val1 = stoi(op1);
+    }
+
+    if (sheet.find(op2) != sheet.end()) {
+        val2 = sheet[op2];
+    } else {
+        bool num = !op2.empty();
+        for (char c : op2) {
+            if (!isdigit(c)) { num = false; break; }
+        }
+        if (num) val2 = stoi(op2);
+    }
+
+    return val1 + val2;
+}
+
 };
+
 
 
 /**

19e3d6800eacb915a60478613f1be4e99e2207f7
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-10-02 19:07:16 +0530

    Time: 55 ms (5.04%) | Memory: 17.2 MB (46.39%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+        int profit=0;
+        int curr_min=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int cost = nums[i]-curr_min;
+            cout << cost << " ";
+            if(cost>0){
+                profit+=cost;
+                curr_min = INT_MAX;
+            }
+            curr_min=min(curr_min,nums[i]);
+        }
+        return profit;
+    }
+};

fbb529251b37a63658c9d13256b536d34032286d
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-10-17 01:01:51 +0530

    Time: 7 ms (15.59%) | Memory: 15.7 MB (11.03%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool makeEqual(vector<string>& words) {
-    unordered_set<string>s(words.begin(),words.end());
-    if(s.size()==1) return true;
      unordered_map<char,int>mpp;
      for(string s:words){
         for(char c:s){

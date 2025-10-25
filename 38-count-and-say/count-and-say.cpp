c43aae177151d10603c70010a89fdb19ada112a4
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-10-25 16:09:06 +0530

    Time: 58 ms (5.03%) | Memory: 125.1 MB (5.15%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        string curr = "1";
+        n--;
+        while(n--){
+            string res="";
+            int cnt=1;
+            for(int i=1;i<curr.size();i++){
+                if(curr[i] == curr[i-1]){
+                    cnt++;
+                }
+                else{
+                    res = res + to_string(cnt) + curr[i-1];
+                    cnt=1;
+                }
+            }
+            res = res + to_string(cnt) + curr.back();
+            cout << res << " " << n << endl;
+            curr = res; 
+        }
+        return curr;
+    }
+};

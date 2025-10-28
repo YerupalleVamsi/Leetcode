5c4a794f431802263c14c9918e73be83255c6bab
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-10-28 21:59:50 +0530

    Time: 1 ms (82.39%) | Memory: 18.3 MB (58.19%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int N, vector<vector<int>>& pre) {
+        vector<vector<int>>adj(N);
+        for(auto &it:pre){
+            adj[it[1]].push_back(it[0]); 
+        }
+        vector<int>res;
+        vector<int>indeg(N,0);
+        queue<int>q;
+        for (int u = 0; u < N; u++) {
+        for (auto v : adj[u]) {
+            indeg[v]++;
+        }
+        }
+
+        for(int i=0;i<N;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            res.push_back(curr);
+            for(auto &it:adj[curr]){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(res.size()==N){
+            return res;
+        }
+        vector<int>r;
+        return r;
+    }
+};

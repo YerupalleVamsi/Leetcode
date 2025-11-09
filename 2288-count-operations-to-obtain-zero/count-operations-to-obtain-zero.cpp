2b7494d6e8130bf9832702bed8195125ed981719
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-11-09 07:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.74%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        long long op=0;
+        while(num1!=0 && num2!=0){
+            op++;
+            if(num1>=num2){
+                num1-=num2;
+            }
+            else
+            num2-=num1;
+        
+        }
+        return op;
+    }
+};

fcb482a0c12ce8b621cff084229e6d108e05b294
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-11-11 00:25:41 +0530

    Time: 454 ms (19.94%) | Memory: 366.8 MB (15.17%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums) {
-        stack<int>st;
-        int res=0;
-        for(int i:nums){
-            while(!st.empty() && st.top() > i){
-                st.pop();
-            }
-            if(i == 0) continue;
-            if(st.empty() || st.top() < i){
-            res++;
-            st.push(i);
-            }
+    int n;
+    vector<long long> tree;
+    void build(vector<int>& nums, int node, int start, int end) {
+        if (start == end)
+            tree[node] = nums[start];
+        else {
+            int mid = (start + end) / 2;
+            build(nums, 2 * node + 1, start, mid);
+            build(nums, 2 * node + 2, mid + 1, end);
+            tree[node] = min(tree[2 * node + 1], tree[2 * node + 2]);
         }
-        return res;
+    }
+    long long query(int node, int start, int end, int l, int r) {
+        if (start > r || end < l) return LLONG_MAX;
+        if (start >= l && end <= r) return tree[node];
+        int mid = (start + end) / 2;
+        return min(
+            query(2 * node + 1, start, mid, l, r),
+            query(2 * node + 2, mid + 1, end, l, r)
+        );
+    }
+
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        tree.resize(4 * n, 0);
+        build(nums, 0, 0, n - 1);
+        int op = 0;
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) continue;
+            if (mpp.find(nums[i]) == mpp.end()) {
+                op++;
+            }
+            else if (query(0,0,n-1,mpp[nums[i]]+1,i-1) < nums[i]) {
+                op++;
+            }
+            mpp[nums[i]] = i;
+        }
+        return op;
     }
 };

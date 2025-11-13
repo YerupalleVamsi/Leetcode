34223d7267e9adf59b4d1c59c118fe29a90aa9c0
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-11-13 15:10:52 +0530

    Time: 7 ms (43.12%) | Memory: 15.8 MB (53.99%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int ones = 0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='1') ones++;
-        }
-        bool allones = true;
-        int cnt1=0;
-        for(int i=s.size()-1;i>=0;i--){
-            if(s[i]=='0' && cnt1!=ones){
-                allones=false;
-                break;
-            }
-            else if(s[i]=='1'){
-                cnt1++;
-            }
-        }
-        if(allones) return 0;
-        int cnt2=0;
+        int cnt=0;
         int res=0;
         bool taken =false;
         for(int i=0;i<s.size();i++){
             
             if(s[i]=='1'){
                 taken = true;
-                cnt2++;
+                cnt++;
             }
             if(s[i]=='0' && taken){ 
-                res+=cnt2;
+                res+=cnt;
                 taken = false;}
         }
         return res;

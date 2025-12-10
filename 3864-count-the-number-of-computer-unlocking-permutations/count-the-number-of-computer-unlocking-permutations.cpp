3a94c202a5a5e7a8a86b5ae17359f6a990fe407f
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-12-10 06:38:34 +0530

    Time: 0 ms (100.00%) | Memory: 92.9 MB (47.00%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     const int MOD = 1e9+7;
-   long long fac(int n) {
-        long long ans = 1;
-        for (int i = 2; i <= n; i++) {
-            ans = (ans * i) % MOD;
-        }
-        return ans;
-    }
     int countPermutations(vector<int>& complexity) {
-    int first = complexity[0];
-    sort(complexity.begin(),complexity.end());
-    int cnt=0;
-    for(int i=0;i<complexity.size();i++){
-        if(complexity[i]==first) cnt++;
-        if(cnt>1) return 0;
+    long long res=1;
+    int n=complexity.size();
+    for(int i=1;i<n;i++){
+        if(complexity[i]<=complexity[0]) return 0;
+        res*=i;
+        res=res%MOD;
     }
-    if(first!=complexity[0]) return 0;
-    int n = complexity.size();
-     long long res = fac(n-1);
-     return res%MOD;   
+    return res;
     }
 };

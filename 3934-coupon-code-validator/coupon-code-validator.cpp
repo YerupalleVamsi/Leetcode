08a5adc0bb2229cec954469bb1672fa6cd3c14d4
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-12-13 10:24:00 +0530

    Time: 11 ms (30.04%) | Memory: 71 MB (14.16%) - LeetSync

---
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        // code[i] => [a-z,A-Z,0-9] and['_'];
+        // businessLine[i] => {"electronics", "grocery", "pharmacy", "restaurant"}
+        // isActive[i] is true;
+        unordered_set<string>s = {"electronics", "grocery", "pharmacy", "restaurant"};
+        vector<pair<string,string>> cb;
+        for(int i=0;i<code.size();i++){
+            string curr_code = code[i];
+            string curr_business = businessLine[i];
+            bool isvalid = true;
+            for(auto it:curr_code){
+                if(!isalnum(it) && it!='_') {
+                    isvalid = false;
+                    break;
+                }
+            }
+            if(curr_code.size()==0) isvalid=false;
+            if(isActive[i] && s.count(curr_business) && isvalid ){
+                cb.push_back({curr_business,curr_code});
+            }
+        }
+        vector<string>res;
+        sort(cb.begin(),cb.end());
+        for(auto it: cb){
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};

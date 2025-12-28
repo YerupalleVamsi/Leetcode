928e0280f2cf493bcda36129c1414ca68377282f
vamsi <147685700+YerupalleVamsi@users.noreply.github.com>
2025-12-28 20:35:10 +0530

    Time: 7 ms (89.66%) | Memory: 8.5 MB (72.41%) - LeetSync

---
+++ 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAllOneMultiple(int k) {
+        if(k%2==0 || k%5==0) return -1;
+      int st = 1;
+      int d = 1;
+      while(1)
+      {
+        int rem = st%k;
+        if(rem == 0){
+            return d;
+        }
+        st = (st*10 + 1) % k;
+        d++;
+      }
+      return 0; 
+    }
+    
+};
